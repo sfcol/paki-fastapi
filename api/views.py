764c8628ad9af74fdc30b1a8d1f8496b96406711
Julian Feinauer <j.feinauer@pragmaticminds.de>
2020-11-21 16:49:37 +0100

    Minor adjustments.

--- api/views.py
+++ api/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
 from api.models import Product
 
 
 def home(request):
-   list = Product.objects.all()
-   return render(request, "./api/home.html", {'list': list})
+    products = Product.objects.all()
+    return render(request, "./api/home.html", {'list': products})
